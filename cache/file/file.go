437c31264b33783971444d8b76196479347c3d31
chenlei <leilei1512159@gmail.com>
2020-07-31 17:57:57 +0800

    xx

--- cache/file/file.go
+++ cache/file/file.go
@@ -1,9 +1,25 @@
 package file
 
+import (
+	"errors"
+
+	go_helper "github.com/leychan/go-helper"
+)
+
 func Set(path string, body []byte) (bool, error) {
+	if !go_helper.FileOrDirExists(path) {
+		go_helper.CreateFile(path)
+	}
+	err := go_helper.WriteFile(path, body, "")
+	if err != nil {
+		return false, err
+	}
 	return true, nil
 }
 
 func Get(path string) ([]byte, error) {
-	return []byte{}, nil
+	if !go_helper.FileOrDirExists(path) {
+		return []byte{}, errors.New("no token file")
+	}
+
 }
